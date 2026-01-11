b78ccdee684d5f31b93692a276e6045f1b3da74e
dreik84 <dreik84@mail.ru>
2026-01-11 16:23:13 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Percent.get(25, 10));
+        System.out.println(Percentage.getPercentOfNum(25, 10));
+        System.out.println(Percentage.getNumOfNum(5, 20));
+        System.out.println(Percentage.getPercentOfDiff(50, 20));
+        System.out.println(Percentage.getPercentOfPercent(50, 40));
     }
 }
