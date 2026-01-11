f584ae7b9aa4b7abbe657d7d697c9eb7e7f16047
dreik84 <dreik84@mail.ru>
2026-01-11 16:22:58 +0300

    Rename Percent to Percentage

--- src/main/java/Percent.java
+++
@@ -1,5 +0,0 @@
-public class Percent {
-    public static double get(double num, double percent) {
-        return percent / 100 * num;
-    }
-}
---
+++ src/main/java/Percentage.java
@@ -0,0 +1,18 @@
+public class Percentage {
+
+    public static double getPercentOfNum(double num, double percent) {
+        return percent * num / 100;
+    }
+
+    public static double getNumOfNum(double num1, double num2) {
+        return num1 / num2 * 100;
+    }
+
+    public static double getPercentOfDiff(double num, double diff) {
+        return num * (100 + diff) / 100;
+    }
+
+    public static double getPercentOfPercent(double percent1, double percent2) {
+        return (percent1 / 100) * (percent2 / 100) * 100;
+    }
+}
